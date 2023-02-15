d2390b2ca3b582b7ac12de0942c18eab622d39a9
rohitkbc <rohitkbc@gmail.com>
2023-02-15 22:31:01 +0530

    first commit

---
+++ src/test/java/com/test/TestDemo.java
@@ -0,0 +1,61 @@
+package com.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestDemo {
+	
+	WebDriver driver;
+	
+	@Before
+	public void beforePre() {
+		System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+        driver = new ChromeDriver();
+	}
+	
+	@Test
+	public void firstTest()
+    {
+        driver.get("https://www.saucedemo.com/");
+    }
+	
+	@Test
+    public void smarterTest()
+    {
+        driver.get("https://www.saucedemo.com/");
+        assertEquals("Swag Labs", driver.getTitle());
+    }
+	
+	@Test
+	public void orangeHRM() {
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        assertEquals("OrangeHRM", driver.getTitle());
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void orangeHRMLogin() {
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.findElement(By.name("username")).sendKeys("Admin");
+        driver.findElement(By.name("password")).sendKeys("admin123");
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")).sendKeys(Keys.ENTER);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        String text = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[1]/header/div[1]/div[1]/span/h6")).getText();
+        assertEquals("Dashboard", text);
+	}
+	
+	@After
+	public void afterEnd() {
+		driver.quit();
+	}
+}
